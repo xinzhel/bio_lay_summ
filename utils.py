56b12830252d59ede42fc9586a72023d358f3526
xinzhe <sergioli212@outlook.com>
2023-03-20 21:15:07 +1100

    daily update

---
+++ utils.py
@@ -0,0 +1,16 @@
+import json
+
+def read_data(file_path):
+    with open(file_path, 'r') as f:
+        lst = []
+        for line in f.readlines():
+            text = json.loads(line)
+            lst.append(text)
+    print("Read data from : ", file_path)
+    print("The number of data: ", len(lst))
+
+    # articles and summaries
+    articles = [example['article'] for example in lst]
+    summaries = [example['lay_summary'] for example in lst]
+
+    return articles, summaries
