f241d7903645daa20e301f131ada7a1604a9808e
xinzhe <sergioli212@outlook.com>
2023-04-08 21:07:13 +1000

    batching for training

--- utils.py
+++ utils.py
@@ -14,3 +14,13 @@ def read_data(file_path):
     summaries = [example['lay_summary'] for example in lst]
 
     return articles, summaries
+
+def read_instances(file_path):
+    with open(file_path, 'r') as f:
+        lst = []
+        for line in f.readlines():
+            text = json.loads(line)
+            lst.append(text)
+    print("Read data from : ", file_path)
+    print("The number of data: ", len(lst))
+    return lst
